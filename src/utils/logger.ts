eb06ab342d724b032f96e03965d8eab39129e252
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-08-24 21:28:27 +0800

    ⚙️ chore(logger): switch to synchronous logger initialization

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,9 @@
-import { configure, getConsoleSink, getLogger } from '@logtape/logtape';
+import { configureSync, getConsoleSink, getLogger } from '@logtape/logtape';
 import { prettyFormatter } from '@logtape/pretty';
 
-export async function initLogger() {
-  await configure({
-    sinks: { console: getConsoleSink({ formatter: prettyFormatter }) },
-    loggers: [{ category: 'my-app', lowestLevel: 'debug', sinks: ['console'] }],
-  });
-}
+configureSync({
+  sinks: { console: getConsoleSink({ formatter: prettyFormatter }) },
+  loggers: [{ category: 'my-app', lowestLevel: 'debug', sinks: ['console'] }],
+});
 
 export const createLogger = (...categoryParts: string[]) => getLogger(['my-app', ...categoryParts]);
